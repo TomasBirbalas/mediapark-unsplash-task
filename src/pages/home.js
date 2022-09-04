6e2ceb1799cb97bdd8fd63be194f4c2db0c81d0d
tomasbir <tomas.birbalas@gmail.com>
2022-09-04 16:01:33 +0300

    added stylesheet

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@ import Content from "../components/Content";
 import Header from "../components/Header";
 import { DataProvider } from "../SearchContext";
 
+import "../stylesheets/css/home.min.css";
+
 function Home() {
   return (
     <>
