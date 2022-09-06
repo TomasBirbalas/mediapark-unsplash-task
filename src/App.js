d98f82cd03eac99626577e29eb91290befce933d
tomasbir <tomas.birbalas@gmail.com>
2022-09-06 10:37:08 +0300

    New page Favorite images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { FavoriteImages } from "./pages/FavoriteImages";
 import { NotFound } from "./pages/NotFound";
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" exact element={<Home />}></Route>
+          <Route
+            path="/FavoriteImages"
+            exact
+            element={<FavoriteImages />}
+          ></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </Router>
